af33f7ad51b16c0a80f14a52bbb019e33d7fdd40
YC815 <115082377+YC815@users.noreply.github.com>
2025-03-05 18:58:55 +0800

    Update Tetris99.js

--- Tetris99.js
+++ Tetris99.js
@@ -1,17 +1,12 @@
 line = 0;
-$("#line").text(line);
 function fillLine(y) {
     for (let x = 0; x < countX; x++) {
         board[x + y * countX] = 0;
     }
     line++;
     down()
-    $("#line").text(line);
     check();
-
     if (line >= 18) return;
-
     setTimeout(() => fillLine(y + 1), 500);
 }
-
 fillLine(10);
